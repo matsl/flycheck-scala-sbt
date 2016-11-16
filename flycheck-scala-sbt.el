d0c5ad3357b281968281cb45da2cda9bd6f88db4
Robert J. Macomber <robertm@dilithium>
2016-11-16 09:00:57 -0800

    Detect build script failures

--- flycheck-scala-sbt.el
+++ flycheck-scala-sbt.el
@@ -15,38 +15,61 @@
 (require 'sbt-mode)
 (require 'cl-lib)
 
+(defun flycheck-scala-sbt--build-error-p (line)
+  "Test whether LINE indicates that building the project itself failed"
+  (string= "Project loading failed: (r)etry, (q)uit, (l)ast, or (i)gnore? " line))
+
+(defun flycheck-scala-sbt--default-prompt-detection (line)
+  "Test whether LINE represents an SBT input prompt"
+  (if (flycheck-scala-sbt--build-error-p line)
+      :build-error
+    (string= line "> ")))
+
 (defun flycheck-scala-sbt--wait-for-prompt-then-call (sbt-buffer f)
   "Wait for the SBT prompt in SBT-BUFFER, then call F.
+
+F is called with `t' if a normal prompt was detected or `nil' if
+a build-script error prompt was found.
 
 The function is called with same current buffer as was originally
 active when the original call was made."
   (let ((current-buffer (current-buffer)))
-    (let ((end (with-current-buffer sbt-buffer
-                 (buffer-substring-no-properties (max (point-min) (- (point-max) 2)) (point-max)))))
-      (if (string= end "> ")
-          (funcall f)
-        (run-at-time 0.1 nil (lambda ()
-                               (with-current-buffer current-buffer
-                                 (flycheck-scala-sbt--wait-for-prompt-then-call sbt-buffer f))))))))
+    (let ((last-line (with-current-buffer sbt-buffer
+                       (save-excursion
+                         (goto-char (point-max))
+                         (buffer-substring-no-properties (line-beginning-position) (line-end-position))))))
+      (case (flycheck-scala-sbt--default-prompt-detection last-line)
+        ((:build-error) (funcall f nil))
+        ((nil) (run-at-time 0.1 nil (lambda ()
+                                      (with-current-buffer current-buffer
+                                        (flycheck-scala-sbt--wait-for-prompt-then-call sbt-buffer f)))))
+        (otherwise (funcall f t))))))
 
-(defmacro flycheck-scala-sbt--wait-for-prompt-then (sbt-buffer &rest body)
+(cl-defmacro flycheck-scala-sbt--wait-for-prompt-then ((sbt-buffer &key result) &body body)
   "Wait for the SBT prompt in SBT-BUFFER, then evaluate BODY.
 
 This is just a macro wrapper for `flycheck-scala-sbt--wait-for-prompt-then-call'."
-  `(flycheck-scala-sbt--wait-for-prompt-then-call ,sbt-buffer (lambda () ,@body)))
+  `(flycheck-scala-sbt--wait-for-prompt-then-call ,sbt-buffer (lambda (,(or result (gensym))) ,@body)))
 
 (defun flycheck-scala-sbt--start (checker status-callback)
   "The main entry point for the CHECKER.  Don't call this.  STATUS-CALLBACK."
   (let ((sbt-buffer (save-window-excursion (sbt:run-sbt))))
-    (flycheck-scala-sbt--wait-for-prompt-then sbt-buffer
-      (save-window-excursion
-        (let ((sbt:save-some-buffers nil))
-          (sbt:command "compile" nil)))
-      (flycheck-scala-sbt--wait-for-prompt-then sbt-buffer
-        (condition-case err
-            (funcall status-callback 'finished (flycheck-scala-sbt--errors-list sbt-buffer checker))
-          (error
-           (funcall status-callback 'errored (error-message-string err))))))))
+    (flycheck-scala-sbt--wait-for-prompt-then (sbt-buffer :result found-prompt)
+      (if found-prompt
+          (progn
+            (save-window-excursion
+              (let ((sbt:save-some-buffers nil))
+                (sbt:command "compile" nil)))
+            (flycheck-scala-sbt--wait-for-prompt-then (sbt-buffer :result found-prompt)
+              (if found-prompt
+                  (condition-case err
+                      (funcall status-callback 'finished (flycheck-scala-sbt--errors-list sbt-buffer checker))
+                    (error
+                     (funcall status-callback 'errored (error-message-string err))))
+                (pop-to-buffer sbt-buffer)
+                (funcall status-callback 'errored "Build failed"))))
+        (pop-to-buffer sbt-buffer)
+        (funcall status-callback 'errored "Build failed")))))
 
 (defun flycheck-scala-sbt--errors-list (buffer checker)
   "Find the current list of errors in BUFFER and convert them into errors for CHECKER."
@@ -82,8 +105,8 @@ error occurred."
           (error-column (current-column)))
       (forward-line -2) ;; skip caret and actual erroring line
       (while (not (string-match line-regexp (setq current-line
-                                                  (buffer-substring-no-properties (progn (beginning-of-line) (point))
-                                                                                  (progn (end-of-line) (point))))))
+                                                  (buffer-substring-no-properties (line-beginning-position)
+                                                                                  (line-end-position)))))
         (unless (string-match else-regexp current-line)
           (error "Expected to find an error continuation line"))
         (push (match-string 2 current-line) subsequent-lines)
